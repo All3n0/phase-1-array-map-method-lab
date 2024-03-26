de7badef7149af008a949df28749c723be2bf8b5
ALLAN <allankipr6@gmail.com>
2024-03-26 22:59:50 +0300

    new

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  //here first we copy the values of tutorials then with a variable line create an arrow function.
+  return tutorials.map((line) => {
+    //then we create another constant where we splite the variable values and we key in the""to show where the split should start and end therefore dividing the headers into separate
+    const tokens = line.split(" ");
+    //then we create another constant called capitalizedtoken where we copy values of tokens and in the function we make the charachter in the first index capital and delete the one that was ins smaall case.
+    const capitalizedTokens = tokens.map(
+      (token) => token.charAt(0).toUpperCase() + token.slice(1)
+    );
+    //the final contant joins the lines therefore creating the whole array again.
+    const response = capitalizedTokens.join(" ");
+    return response;
+  })
 }
+
